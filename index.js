f776a742e1e2af63817fd9141e386966521a2c30
Gihan Ubayawardana <gihan.ubayawardana@infor.com>
2019-06-30 02:26:10 +0530

    add npm script

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const x = "hi";
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ res: "200ok" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
