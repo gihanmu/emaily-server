b15343e9a1d7992001d8b2cf3b002152276693b3
Gihan Ubayawardana <gihan.ubayawardana@infor.com>
2019-07-24 20:15:48 +0530

    add passport.js

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
-const app = express();
+const passport = require("passport");
+const GoogleStratrgy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
-app.get("/", (req, res) => {
-  res.send({ res: "200ok" });
-});
+const app = express();
+passport.use(
+    new GoogleStratrgy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback"
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"]
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 
